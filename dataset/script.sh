9ee073efdf246c37a977a045be4c92fd7a5247e9
Chinmay <chinmayjindal108@gmail.com>
2025-07-30 12:17:03 -0400

    add code to run on gpu

---
+++ dataset/script.sh
@@ -0,0 +1,8 @@
+# run this script to convert the pdf (already done)
+
+udo apt-get update
+sudo apt-get install poppler-utils ttf-mscorefonts-installer msttcorefonts fonts-crosextra-caladea fonts-crosextra-carlito gsfonts lcdf-typetools
+conda create -n olmocr python=3.11
+conda activate olmocr
+pip install olmocr
+python -m olmocr.pipeline ./converted --markdown --pdfs ConciseRevised.pdf
