b47db3e4b5dcabc77a12459c6834094cb74b9d30
Chinmay <chinmayjindal108@gmail.com>
2025-07-30 12:25:29 -0400

    gpu code

--- dataset/script.sh
+++ dataset/script.sh
@@ -2,7 +2,7 @@
 
 udo apt-get update
 sudo apt-get install poppler-utils ttf-mscorefonts-installer msttcorefonts fonts-crosextra-caladea fonts-crosextra-carlito gsfonts lcdf-typetools
-conda create -n olmocr python=3.11
-conda activate olmocr
+# conda create -n olmocr python=3.11
+# conda activate olmocr
 pip install olmocr
 python -m olmocr.pipeline ./converted --markdown --pdfs ConciseRevised.pdf
